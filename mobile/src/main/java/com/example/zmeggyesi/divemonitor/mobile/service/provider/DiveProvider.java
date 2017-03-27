c391d8b611ad713d5ddf25681afbcdec79fbb4e7
Zalán Meggyesi <zmeggyesi@skawa.hu>
2017-03-27 08:43:09 +0200

    prototype content provider

---
+++ mobile/src/main/java/com/example/zmeggyesi/divemonitor/mobile/service/provider/DiveProvider.java
@@ -0,0 +1,88 @@
+package com.example.zmeggyesi.divemonitor.mobile.service.provider;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.text.TextUtils;
+
+import com.example.zmeggyesi.divemonitor.mobile.model.GlobalContext;
+
+/**
+ * Created by zmeggyesi on 2017. 03. 27..
+ */
+
+public class DiveProvider extends ContentProvider {
+	private static final UriMatcher URI_MATCHER = new UriMatcher(UriMatcher.NO_MATCH);
+	private GlobalContext gc;
+	private SQLiteDatabase divesDB;
+
+	static {
+		URI_MATCHER.addURI("com.example.zmeggyesi.divemonitor.provider", "dives", 1);
+		URI_MATCHER.addURI("com.example.zmeggyesi.divemonitor.provider", "dives/#", 2);
+	}
+
+	public DiveProvider() {}
+
+	@Override
+	public boolean onCreate() {
+		gc = (GlobalContext) getContext();
+		divesDB = gc.getDivesDatabase(false);
+		return true;
+	}
+
+	@Nullable
+	@Override
+	public Cursor query(@NonNull Uri uri,
+	                    @Nullable String[] projection,
+	                    @Nullable String selection,
+	                    @Nullable String[] selectionArgs,
+	                    @Nullable String sortOrder) {
+		switch (URI_MATCHER.match(uri)) {
+			case 1 :
+				// Multi-row query
+				if (TextUtils.isEmpty(sortOrder)) {
+					sortOrder = "_ID ASC";
+				}
+				break;
+			case 2 :
+				// Single-row query
+				selection = selection + "_ID = " + uri.getLastPathSegment();
+				break;
+			default :
+				throw new IllegalArgumentException("URI not recognized: " + uri);
+		}
+
+		return divesDB.query("dives", projection, selection, selectionArgs, null, null, sortOrder);
+	}
+
+	@Nullable
+	@Override
+	public String getType(@NonNull Uri uri) {
+		switch (URI_MATCHER.match(uri)) {
+			case 1 : return "vnd.android.cursor.dir/vnd.com.example.zmeggyesi.divemonitor.dive";
+			case 2 : return "vnd.android.cursor.item/vnd.com.example.zmeggyesi.divemonitor.dive";
+			default : throw new UnsupportedOperationException("Requested URI does not match: " + uri);
+		}
+	}
+
+	@Nullable
+	@Override
+	public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+		throw new UnsupportedOperationException("Provider is read-only!");
+	}
+
+	@Override
+	public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+		throw new UnsupportedOperationException("Provider is read-only!");
+	}
+
+	@Override
+	public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+		throw new UnsupportedOperationException("Provider is read-only!");
+	}
+}
